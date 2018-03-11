ca570e53dec892afeb72ca1e887f3a1066533b64
Wei Dong <wdong@wdong.org>
2018-03-11 09:06:24 -0400

    update

--- draw_ranks.py
+++ draw_ranks.py
@@ -4,20 +4,20 @@ import cv2
 
 
 H = 60
-W = 1200
+W = 2000
 with open('ranks/index.html', 'w') as index:
     index.write('<HTML><BODY>\n')
-    for l in open(interest, 'r'):
+    for l in open('interest', 'r'):
         gene, probe = l.strip().split('\t')
 
         image = np.zeros((H, W), dtype=np.uint8)
         with open("ranks/%s_%s" % (gene, probe), 'r') as f:
             for v in f:
-                v = int(round(float(v.strip())))
-                cv2.line(image, (v, 0), (v, H), 255, 2)
+                v = int(round(W * float(v.strip())))
+                cv2.line(image, (v, 0), (v, H), 255, 1)
                 pass
             pass
-        cv2.imwrite(image, "ranks/%s_%s.png" % (gene, probe))
+        cv2.imwrite("ranks/%s_%s.png" % (gene, probe), image)
         index.write("<h2>%s - %s</h2>\n" % (gene, probe))
         index.write("<img src='%s_%s.png'></img><br/><hr/>\n" % (gene,probe))
         pass
