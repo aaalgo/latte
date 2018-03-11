2ddc39d1c64d1e8249e39d4e7d1a75d4e268f5da
Wei Dong <wdong@wdong.org>
2018-03-11 00:52:28 -0500

    add

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = g++
-CXXFLAGS += -O3 -g -std=c++11 -I/usr/include/python2.7 -Iaffy/sdk/ -D_INCLUDE_UNISTD_HEADER_=1
-LDFLAGS += -L/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu -L/usr/lib/python2.7/config-x86_64-linux-gnu/
+CXXFLAGS += -O3 -g -std=c++11 -I/usr/include/python2.7 -Iaffy/sdk/ -D_INCLUDE_UNISTD_HEADER_=1 -fopenmp
+LDFLAGS += -L/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu -L/usr/lib/python2.7/config-x86_64-linux-gnu/ -fopenmp
 LDLIBS += -lboost_program_options -lboost_numpy -lboost_python -lpython2.7
 
 AFFY_SRCS = affy/sdk/file/FileIO.cpp \
@@ -10,7 +10,7 @@ AFFY_SRCS = affy/sdk/file/FileIO.cpp \
 AFFY_OBJS = $(AFFY_SRCS:.cpp=.o)
 
 
-all:	pack compare 
+all:	pack compare extract_ranks
 
 
 cdfcel:	cdfcel.o $(AFFY_OBJS)
---
+++ draw_ranks.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import numpy as np
+import cv2
+
+
+H = 60
+W = 1200
+with open('ranks/index.html', 'w') as index:
+    index.write('<HTML><BODY>\n')
+    for l in open(interest, 'r'):
+        gene, probe = l.strip().split('\t')
+
+        image = np.zeros((H, W), dtype=np.uint8)
+        with open("ranks/%s_%s" % (gene, probe), 'r') as f:
+            for v in f:
+                v = int(round(float(v.strip())))
+                cv2.line(image, (v, 0), (v, H), 255, 2)
+                pass
+            pass
+        cv2.imwrite(image, "ranks/%s_%s.png" % (gene, probe))
+        index.write("<h2>%s - %s</h2>\n" % (gene, probe))
+        index.write("<img src='%s_%s.png'></img><br/><hr/>\n" % (gene,probe))
+        pass
+    index.write("</BODY></HTML>\n")
+    pass
+
+
+
